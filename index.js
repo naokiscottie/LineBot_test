ab34ceca724073bbcac4cf7b42cb0c40bde8a731
naokiscottie <naokiscottie5@gmail.com>
2023-05-24 08:26:46 +0900

    test_commit

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const express = require("express");
+const line = require("@line/bot-sdk");
+const PORT = process.env.PORT || 3000;
+
+const getChannelSeacret = "ご自身のchannel seacret key";
+const getChannelAccessToken = "ご自身のchannel accsess token key";
+
+const config = {
+  channelSecret: getChannelSeacret(),
+  channelAccessToken: getChannelAccessToken(),
+};
+
+const app = express();
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  console.log(req.body.events);
+
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  /* -------------------------------------------
+    ------- 返信メッセージの処理の記述ゾーン ---------
+    ------------------------------------------- */
+
+  let replyText = "";
+  replyText = event.message.text; // event.message.textに自分のメッセージが入っている。
+
+  /* ------------------------------------------
+    ------------------------------------------
+    ------------------------------------------ */
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: replyText,
+  });
+}
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
